ffb35cd21330691a0fcb34492a8bac8ec7c7e7ab
Tom Hutchings <goerofmuns@gmail.com>
2017-03-31 16:24:56 +0100

    added multiple monitor support!

--- wall.py
+++ wall.py
@@ -8,20 +8,18 @@ from Foundation import NSURL
 
 ws = NSWorkspace.sharedWorkspace()
 
-#print(file_url)
-
 options = {}
-
-#for screen in NSScreen.screens():
-
 screens = NSScreen.screens()
-
 args = sys.argv
 args.pop(0)
 
-for index, wall in enumerate(args):
-    file_path = os.path.abspath(wall)
-
-    file_url = NSURL.fileURLWithPath_(file_path)
-
-    (result, error) = ws.setDesktopImageURL_forScreen_options_error_(file_url, screens[index], options, None)
+if (len(args) > 1):
+    for index, wall in enumerate(args):
+        file_path = os.path.abspath(wall)
+        file_url = NSURL.fileURLWithPath_(file_path)
+        (result, error) = ws.setDesktopImageURL_forScreen_options_error_(file_url, screens[index], options, None)
+else:
+    for screen in screens:
+        file_path = os.path.abspath(args[0])
+        file_url = NSURL.fileURLWithPath_(file_path)
+        (result, error) = ws.setDesktopImageURL_forScreen_options_error_(file_url, screen, options, None)
